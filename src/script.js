a215e805d43d5764252e79ae8a810f1bf194bf61
thepigcat <benpospo@gmail.com>
2023-11-23 19:02:01 +0100

    polishing

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 var pMenu = document.getElementById("projects-menu");
 var hMenu = document.getElementById("hobbies-menu");
 var selectionBar = document.getElementById("selection-bar");
-var followDiv = document.getElementById('cursor-following');
+var followDiv = document.getElementById("cursor-following");
 
 function closeMenu() {
   pMenu.style.display = "none";
@@ -19,8 +19,41 @@ function openHobbiesMenu() {
   hMenu.style.display = "flex";
 }
 
-document.addEventListener('mousemove', function(event) {
+window.onmousemove = (event) => {
   followDiv.style.display = "block"
-  followDiv.style.left = (event.clientX - followDiv.offsetWidth / 2) + 'px';
-  followDiv.style.top = (event.clientY - followDiv.offsetHeight / 2) + 'px';
-})
+  const x = event.clientX - followDiv.offsetWidth / 2 + "px";
+  followDiv.style.left = x;
+  const y = event.clientY - followDiv.offsetHeight / 2 + "px";
+  followDiv.style.top = y;
+
+  var cursorType =
+    event.target.style.cursor || getComputedStyle(event.target).cursor;
+
+  var isTextMode = cursorType === "text";
+
+  console.log("cursor type: " + cursorType);
+
+  // Log the result to the console
+  console.log("Is in text mode:", isTextMode);
+
+  var elementUnderCursor = document
+    .elementFromPoint(event.clientX, event.clientY)
+    .tagName.toLowerCase();
+
+  if (
+    elementUnderCursor == "p" ||
+    elementUnderCursor == "li" ||
+    elementUnderCursor == "ul" ||
+    elementUnderCursor == "a"
+  ) {
+    followDiv.style.opacity = '0'
+    console.log("over text")
+  } else {
+    followDiv.style.opacity = '1'
+  }
+};
+
+window.onmouseout = (event) => {
+    followDiv.style.display = "none"
+    console.log("left window")
+}
