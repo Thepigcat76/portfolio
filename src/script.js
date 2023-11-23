82f78b0cf816a3518e542b0230655a722ee7dcd8
thepigcat <benpospo@gmail.com>
2023-11-23 16:06:25 +0100

    update projects

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 var pMenu = document.getElementById("projects-menu");
 var hMenu = document.getElementById("hobbies-menu");
 var selectionBar = document.getElementById("selection-bar");
+var followDiv = document.getElementById('cursor-following');
 
 function closeMenu() {
   pMenu.style.display = "none";
@@ -17,3 +18,9 @@ function openHobbiesMenu() {
   selectionBar.style.display = "none";
   hMenu.style.display = "flex";
 }
+
+document.addEventListener('mousemove', function(event) {
+  followDiv.style.display = "block"
+  followDiv.style.left = (event.clientX - followDiv.offsetWidth / 2) + 'px';
+  followDiv.style.top = (event.clientY - followDiv.offsetHeight / 2) + 'px';
+})
